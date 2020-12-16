ec8a29e001e6a3cf5cc6664cd05e2034fa02d312
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-12-17 00:24:24 +0800

    Update home.php

--- home.php
+++ home.php
@@ -9,7 +9,8 @@
     </style>
     <body>
         <div id="content1">
-            <div class="chonkobo">              
+            <div class="chonkobo">
+                <object type="image/svg+xml" data="svg/tet.svg"></object>   
             </div>
             <div class="header header-anim">Christian Dominic Ledesma</div>
             <div class="header">BSIT3A</div>
